968b25b2b3fc7fff73efbd7d76895eb6ade6c751
Aleksey_Morgachev <morgachev_aleksey@mail.ru>
2026-03-31 14:41:32 +0300

    v.1.3 Начата реализация репозитория

--- src/main/java/com/example/blog/App.java
+++ src/main/java/com/example/blog/App.java
@@ -3,6 +3,6 @@ package com.example.blog;
 //Точка входа сценария
 public class App {
     public static void main(String[] args) {
-
+        //TODO Реализовать метод создания файла-хранилища
     }
 }
--- src/main/java/com/example/blog/repository/FilePostRepository.java
+++ src/main/java/com/example/blog/repository/FilePostRepository.java
@@ -1,6 +1,60 @@
 package com.example.blog.repository;
 
-//Класс для хранения/работы с Постами
-public class FilePostRepository {
+import com.example.blog.model.Post;
+import java.util.*;
+import java.io.*;
 
+//Класс для хранения/работы с Постами
+public class FilePostRepository implements PostRepository {
+    //Поля репозитория
+    private final String fileName;
+    private final List<Post> posts;
+    private long currentId;
+
+    //Конструктор
+    public FilePostRepository(String fileName) {
+        this.fileName = fileName;
+        this.posts = new ArrayList<>();
+        this.currentId = 0;
+        loadFromFile();
+    }
+
+    //Загрузить данные их файла
+    private void loadFromFile(){
+        //1)Проверить существует ли файл
+        //2)Прочитать посты и положить в posts
+        //3)Максимальный id сохранить в currentId
+        File file = new File(fileName); //Обьект для описания пути к файлу - фактически ничего не делает
+        if (!file.exists()){
+            currentId = 0;
+            return;
+        } //Выходим из метода если файл не существует (но он существует т.к. будет создаваться на этапе запуска проекта)
+
+    }
+
+    //Сохранить данные в файл
+    private void saveToFile(){
+
+    }
+
+    //Переопределение основных методов
+    @Override
+    public Post save(Post post){
+        return post;
+    }
+
+    @Override
+    public void deleteById(Long id){
+
+    }
+
+    @Override
+    public Optional<Post> findById (Long Id){
+        return Optional.empty();
+    }
+
+    @Override
+    public List<Post> findAll(){
+        return List.of();
+    }
 }
